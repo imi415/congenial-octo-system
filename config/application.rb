6d8dce5c781355dc9a05169ddfd56531273959f8
imi415 <imi415.public@gmail.com>
2018-11-01 01:07:08 +0800

    Fixed interal IP addresses.

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,8 @@ module Live
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-
+    config.action_dispatch.trusted_proxies = %w(127.0.0.1).map { |proxy| IPAddr.new(proxy) }  # localhost
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
