3edb8c4de64a3cff34a7741fdfb5716c7324b471
imi415 <imi415.public@gmail.com>
2018-11-25 16:17:47 +0800

    Use ENV instead of getting from stream.

--- php/on_publish.php
+++ php/on_publish.php
@@ -5,14 +5,14 @@ $app = $_POST['app'];
 $name = $_POST['name'];
 $key = $_POST['key'];
 
-$redis_key = $app . '_' . $name . '_key';
+$redis_key = $_ENV['LIVE_NAME'] . '_' . $name . '_key';
 
 $key_from_redis = $redis -> get($redis_key);
 
 //error_log($key . ' ' . $key_from_redis, 0);
 
 if ($key_from_redis === $key) {
-    $status_key = $app . '_' . $name . '_status';
+    $status_key = $_ENV['LIVE_NAME'] . '_' . $name . '_status';
     $redis ->set($status_key, 'on');
 }
 else {
--- php/on_publish_done.php
+++ php/on_publish_done.php
@@ -4,6 +4,6 @@ require_once('inc.php');
 $app = $_POST['app'];
 $name = $_POST['name'];
 
-$status_key = $app . '_' . $name . '_status';
+$status_key = $_ENV['LIVE_NAME'] . '_' . $name . '_status';
 $redis ->set($status_key, 'off');
 
